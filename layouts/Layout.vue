8dd3f018851b040f1117957f54306be310c32ba8
Anc <anc136k@gmail.com>
2020-03-25 17:29:13 +0800

    feat: add simple night mode \ simple append css invert to html dom so if using tag <a> it will refresh otherwise <route-link>

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -8,32 +8,57 @@
       >
         {{ $site.title }}
       </router-link>
-      <div style="clear: both" />
-      <nav-bar />
+      <div style="clear: both"/>
+      <nav-bar/>
+      <div class="icon-night" @click="nightMode"></div>
     </header>
-    <home-page v-if="isHome" />
-    <post-page v-else />
-    <footer-bar />
+    <home-page v-if="isHome"/>
+    <post-page v-else/>
+    <footer-bar/>
   </div>
 </template>
 
 <script>
-import HomePage from "../components/Home";
-import PostPage from "../components/Post";
-import FooterBar from '../components/FooterBar';
-import NavBar from '../components/NavBar';
+    import HomePage from "../components/Home";
+    import PostPage from "../components/Post";
+    import FooterBar from '../components/FooterBar';
+    import NavBar from '../components/NavBar';
 
-export default {
-  components: {
-    HomePage,
-    PostPage,
-    FooterBar,
-    NavBar,
-  },
-  computed: {
-    isHome() {
-      return this.$page.path === '/';
-    }
-  }
-};
+    export default {
+        components: {
+            HomePage,
+            PostPage,
+            FooterBar,
+            NavBar,
+        },
+        computed: {
+            isHome() {
+                return this.$page.path === '/';
+            },
+        },
+        methods: {
+            nightMode() {
+                var styleElement = document.getElementById('styles_js');
+                if (!styleElement) {
+                    addNewStyle('body {background-color:white;filter: brightness(90%) invert(100%) hue-rotate(180deg);-webkit-font-smoothing: antialiased;}');
+                    addNewStyle('html {background-color:#191919;}');
+                    addNewStyle('picture,video,img {filter: hue-rotate(180deg) invert(100%) brightness(111.111%);}');
+                } else {
+                    document.getElementsByTagName('head')[0].removeChild(styleElement);
+                }
+
+                function addNewStyle(newStyle) {
+                    var styleElement = document.getElementById('styles_js');
+                    if (!styleElement) {
+                        styleElement = document.createElement('style');
+                        styleElement.type = 'text/css';
+                        styleElement.id = 'styles_js';
+                        document.getElementsByTagName('head')[0].appendChild(styleElement);
+                    }
+
+                    styleElement.appendChild(document.createTextNode(newStyle));
+                }
+            }
+        }
+    };
 </script>
