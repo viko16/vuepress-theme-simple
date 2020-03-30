29d1930ab3b27adee0b0f0aa4d4a8e079604df66
Anc <anc136k@gmail.com>
2020-03-30 14:36:57 +0800

    fix: lang & paging option

--- components/Home.vue
+++ components/Home.vue
@@ -5,14 +5,19 @@
       v-if="filteredList.length === 0"
       class="empty-list"
     >
-      Ooops! Nothing here..🙈
+      <span v-if="this.$lang === 'zh-CN'">
+        落了片白茫茫大地真干净。
+      </span>
+      <span v-else>
+        Ooops! Nothing here..🙈
+      </span>
     </div>
     <ol
       v-else
       class="list"
     >
       <li
-        v-for="page of filteredList"
+        v-for="page of paginatedData"
         :key="page.key"
         class="list-item"
       >
@@ -29,6 +34,21 @@
         />
       </li>
     </ol>
+
+    <div class="pageControl" v-show="isPaging">
+      <button
+        :disabled="pageNumber === 0"
+        @click="prevPage">
+        Prev
+      </button>
+      <button
+        :disabled="pageNumber >= pageCount -1"
+        @click="nextPage">
+        Next
+      </button>
+    </div>
+
+
   </div>
 </template>
 
@@ -36,21 +56,56 @@
     import TimeAgo from './TimeAgo';
     import SearchBox from '@SearchBox';
 
-export default {
-  components: {
-    TimeAgo
-  },
-  computed: {
-    filteredList() {
-      // Order by publish date, desc
-      return this.$site.pages
-        .filter(item => item.path !== '/')
-        .sort((a, b) => {
-          return new Date(b.frontmatter.date || b.lastUpdated) - new Date(a.frontmatter.date || a.lastUpdated)
-        })
+    export default {
+        data() {
+            return {
+                pageNumber: 0,
+                isPaging:false
+            }
+        },
+        components: {
+            TimeAgo,
+            SearchBox
+        },
+        mounted() {
+            this.pageNumber = location.hash ? location.hash.split('#')[1] : 0;
+        },
+        computed: {
+            filteredList() {
+                // Order by publish date, desc
+                return this.$site.pages
+                    .filter(item => item.path !== '/' && item.title && !item.frontmatter.hide)
+                    .sort((a, b) => {
+                        return new Date(b.frontmatter.date || b.lastUpdated) - new Date(a.frontmatter.date || a.lastUpdated)
+                    })
+            },
+            paginatedData() {
+                if(!this.$themeConfig.pageSize){
+                    return this.filteredList;
+                }
+                this.isPaging = true;
+                const start = this.pageNumber * (this.$themeConfig.pageSize || 10),
+                    end = start + (this.$themeConfig.pageSize || 10);
+                return this.filteredList.slice(start, end);
+            },
+            pageCount() {
+                let l = this.filteredList.length,
+                    s = (this.$themeConfig.pageSize || 10);
+                return Math.ceil(l / s);
+            },
+        },
+        methods: {
+            nextPage() {
+                this.pageNumber++;
+                location.hash = this.pageNumber;
+            },
+            prevPage() {
+                this.pageNumber--;
+                location.hash = this.pageNumber;
+            }
+        }
+
     }
-  },
-}
 </script>
 <style scoped>
   .pageControl {
--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   title: 'Example Site',
+  lang: 'zh-CN',
   description: 'Example for vuepress-theme-simple',
   theme: path.resolve(__dirname, '../../'),
   themeConfig: {
